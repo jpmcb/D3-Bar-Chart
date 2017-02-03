409ecef3351c623b6b2529e48843d253dede9494
John McBride <jpmmcbride@gmail.com>
2017-02-03 11:24:41 -0700

    d3 bar chart
    A basic bar chart using JSON data pulled from a database & the d3
    Javascript library

---
+++ README.txt
@@ -0,0 +1,4 @@
+# d3 Bar Chart App
+_______
+
+Using the [d3](https://d3js.org/) JavaScript library, this simple application shows the US Gross Domestic product pulled from a [database](https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json)
---
+++ js/index.js
@@ -0,0 +1,37 @@
+var usData;
+var year = [];
+var amount = [];
+
+function runData() {  
+  var svg = d3.select('.chart').append('svg')
+              .attr('height', '400')
+              .attr('width', '850')
+  
+  svg.selectAll('rect')
+      .data(amount)
+      .enter().append('svg:rect')
+        .attr('height', function(d){ return d / 50; }) 
+        .attr('width','2')
+        .attr('x', function(d,i){ return (i*3);})
+        .attr('y', function(d){ return (390 - (d/50)); })
+        .attr('fill','green')
+        .attr('class', 'svg-rect')
+        .append('svg:title').text(function(d,i){ return '$' + d + ' ' + year[i]; })
+}
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = getData;
+
+request.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
+request.send(null);
+
+function getData(data) {
+  if(request.readyState === XMLHttpRequest.DONE){
+    usData = JSON.parse(data.target.response);
+    for(var i = 0; i < usData.data.length; i++) {
+      amount.push(usData.data[i][1]);
+      year.push(usData.data[i][0]);
+    }
+  runData(); //code for creating bar chart
+  } 
+}
